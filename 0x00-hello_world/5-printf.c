d29e13bb3c0c8fb232484dfd1a54001204b70385
Hassan Teslim <hassateslim12@gmail.com>
2022-10-13 15:19:34 -0700

    5-6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, ");
-fprintf(stderr, "[anything]");
+printf("with proper grammar, but the outcome is a piece of art, \n");
+fprintf(stderr, ["Anything"]);
 return (0);
 }
